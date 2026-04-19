355a9c324c8c17df9d070a59a7bce32840fe08af
Chris Zampogna <200652929+ChrisZampogna@users.noreply.github.com>
2026-04-19 13:39:28 -0400

    Add documentation for console.py

--- util/console.py
+++ util/console.py
@@ -1,3 +1,5 @@
 from rich.console import Console
 
+# The idea here is to have one global instance of "console" (i.e. a singleton)
+# This way, we can write to the same console from anywhere in the program
 console = Console()
